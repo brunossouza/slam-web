c2443e71cf96bc91609fb0edf4e3e73824595a58
Bruno Silva <brunos.souza2@gmail.com>
2020-08-14 12:48:29 -0300

    total devices dashboard

--- src/main/java/dev/valhala/slam/controllers/DashboardController.java
+++ src/main/java/dev/valhala/slam/controllers/DashboardController.java
@@ -1,16 +1,28 @@
 package dev.valhala.slam.controllers;
 
+import dev.valhala.slam.repositories.DevicesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping({"/","/home","/dashboard"})
 public class DashboardController {
 
+    private static final String PATH_PAGE = "/pages/dashboard";
+
+    @Autowired
+    private DevicesRepository devicesRepository;
+
     @GetMapping
-    public String dashboard(){
-        return "/pages/dashboard";
+    public ModelAndView dashboard(){
+        ModelAndView view = new ModelAndView(PATH_PAGE);
+
+        view.addObject("totalDevices", devicesRepository.count());
+
+        return view;
     }
 
 }
