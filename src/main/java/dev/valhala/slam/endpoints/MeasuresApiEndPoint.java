9f09cfe78ce9ff0ab5a5c82f68e1cddf32ef6754
Bruno Silva <brunos.souza2@gmail.com>
2020-08-31 20:22:53 -0300

    init charts page

--- src/main/java/dev/valhala/slam/endpoints/MeasuresApiEndPoint.java
+++ src/main/java/dev/valhala/slam/endpoints/MeasuresApiEndPoint.java
@@ -40,7 +40,11 @@ public class MeasuresApiEndPoint {
     @GetMapping
     public ResponseEntity getMonthlyConsuption(){
         Calendar c = Calendar.getInstance();
-        c.add(Calendar.MONTH, -12);
+        try{
+            c.add(Calendar.MONTH, -12);
+        }catch (Exception e){
+            c.add(Calendar.DAY_OF_MONTH, -1);
+        }
         List<MonthlyConsumptionChart> consumptionChartList = measureRepository.getMonthlyConsuption(c);
         return ResponseEntity.ok(consumptionChartList);
     }
